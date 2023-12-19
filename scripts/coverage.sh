27ee1e65e32f0b23f7e13b40e29b4ffd8fd17be8
Luca Bonamini <lucabonamini28@gmail.com>
2023-12-19 11:55:51 +0100

    feat: update coverage generation

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo -e "\033[0;32m\033[1mRunning GCovr analysis ...\033[0m"
+
 if [[ -d "coverage" ]]; then
   rm -rf ./coverage
 fi
 
 mkdir ./coverage
 
-gcovr --gcov-executable gcov-9 -r . --html --html-details -o coverage/coverage.html .
+lcov -c -d . -o coverage.info
+lcov -r coverage.info "/usr*" -o coverage_no_usr.info
+genhtml coverage_no_usr.info --output-directory coverage
